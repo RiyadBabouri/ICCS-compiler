3386d4e6b6aec121aaa9a78c852df0be2f2b78cf
RiyadBabouri <94042352+RiyadBabouri@users.noreply.github.com>
2024-03-25 11:34:44 +0100

    eerste versie transformer

--- src/main/java/nl/han/ica/icss/transforms/Evaluator.java
+++ src/main/java/nl/han/ica/icss/transforms/Evaluator.java
@@ -2,9 +2,7 @@ package nl.han.ica.icss.transforms;
 
 import nl.han.ica.datastructures.IHANLinkedList;
 import nl.han.ica.icss.ast.*;
-import nl.han.ica.icss.ast.literals.PercentageLiteral;
 import nl.han.ica.icss.ast.literals.PixelLiteral;
-import nl.han.ica.icss.ast.literals.ScalarLiteral;
 import nl.han.ica.icss.ast.operations.AddOperation;
 import nl.han.ica.icss.ast.operations.MultiplyOperation;
 import nl.han.ica.icss.ast.operations.SubtractOperation;
@@ -14,17 +12,77 @@ import java.util.LinkedList;
 
 public class Evaluator implements Transform {
 
-    private IHANLinkedList<HashMap<String, Literal>> variableValues;
+    private LinkedList<HashMap<String, Literal>> variableValues;
 
     public Evaluator() {
-        //variableValues = new HANLinkedList<>();
+        variableValues = new LinkedList<>();
     }
 
     @Override
     public void apply(AST ast) {
-        //variableValues = new HANLinkedList<>();
+        variableValues = new LinkedList<>();
+        applyStylesheet(ast.root);
 
     }
 
-    
+    private void applyStylesheet(Stylesheet stylesheet) {
+        applyStyleRule((Stylerule)stylesheet.getChildren().get(0));
+    }
+
+    private void applyStyleRule(Stylerule rule) {
+        for (ASTNode node : rule.getChildren()) {
+            if (node instanceof Declaration) {
+                applyDeclaration((Declaration) node);
+            }
+        }
+    }
+
+    private void applyDeclaration(Declaration declaration) {
+        declaration.expression = calculateExpression(declaration.expression);
+
+    }
+
+    private Expression calculateExpression(Expression expression) {
+        if (expression instanceof Literal) {
+            return (Literal) expression;
+        } else {
+            return evaluateExpression(expression);
+        }
+    }
+
+    private Expression evaluateExpression(Expression expression) {
+        if (expression instanceof AddOperation) {
+            return evaluateAddOperation((AddOperation) expression);
+        } else if (expression instanceof SubtractOperation) {
+            return evaluateSubtractOperation((SubtractOperation) expression);
+        } else if (expression instanceof MultiplyOperation) {
+            return evaluateMultiplyOperation((MultiplyOperation) expression);
+        }
+        else {
+            return expression;
+        }
+    }
+
+    private Expression evaluateMultiplyOperation(MultiplyOperation expression) {
+        // (waarschijnlijk) iets toevoegen over verschillende litterals
+        PixelLiteral left = (PixelLiteral) evaluateExpression(expression.lhs);
+        PixelLiteral right = (PixelLiteral) evaluateExpression(expression.rhs);
+        return new PixelLiteral(left.value * right.value);
+    }
+
+    private Expression evaluateSubtractOperation(SubtractOperation expression) {
+        // (waarschijnlijk) iets toevoegen over verschillende litterals
+        PixelLiteral left = (PixelLiteral) evaluateExpression(expression.lhs);
+        PixelLiteral right = (PixelLiteral) evaluateExpression(expression.rhs);
+        return new PixelLiteral(left.value - right.value);
+    }
+
+    private Expression evaluateAddOperation(Operation expression) {
+        // (waarschijnlijk) iets toevoegen over verschillende litterals
+        PixelLiteral left = (PixelLiteral) evaluateExpression(expression.lhs);
+        PixelLiteral right = (PixelLiteral) evaluateExpression(expression.rhs);
+        return new PixelLiteral(left.value + right.value);
+    }
+
+
 }
